acff38f2d8a01d99049882a2b92a42aa9cb0e277
meher18 <54255014+meher18@users.noreply.github.com>
2020-02-16 16:01:53 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+       
+            <nav class="navbar">
+                <div class="title"> <h1>HR MANAGEMENT SYSTEM</h1></div>
+                <div class="links"><uL ><li><a href="#">HOME</a></li>
+
+                        <li><a href="#">ABOUT US</a>
+                        <li><a href="#">CONTACT US</a></li>
+
+                    </uL>
+
+                </div>
+                
+
+            </nav>
+         <div class="first_container">
+             <div>
+                  ｡◕‿◕｡  Hello There !
+             </div>
+        </div>
+        <div class="second_container">
+            <div >
+                    <h1>HI AND WELCOME TO HR MANAGEMENT SYSTEM</h1>
+            <P>SCROLL BELOW TO SEE WHAT THIS SYSTEM CONTAINS  : ) </P>
+            </div>
+        </div>
+        <div class="third_container">
+         </div>
+        <div class="fourth_container">
+            <a class="admin"href="#">
+
+                                <div class="admin_image">
+                    
+                
+                      <h3>Hello ! ADMIN CLICK HERE TO CHECK YOUR MODULE </h3> 
+                </div>
+             
+            </a>
+            
+       
+            <a class="HR" href="#">
+
+                                <div class="hr_image">
+                    
+                
+                      <h3>Hello !  H R CLICK HERE TO CHECK YOUR MODULE</h3> 
+                </div>
+             
+            </a>
+      
+        </div>
+        <div class="sixth_container">
+           
+        </div>
+        <div class="footer">
+            
+           
+            
+             <div class="copyright">
+                 <marquee> <h4>Copyright © HR managementSystem. All Rights ReservedƉ and ƈContact Us: +91
+        90000 00000</h4></marquee>
+        </div>
+            
+        </div>
+        
+       
+        
+    </body>
+</html>
